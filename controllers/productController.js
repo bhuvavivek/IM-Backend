863cd7261b77b7d58c587a408460f44963e726f7
bhuvavivek <vivekpate26@gmail.com>
2025-03-08 16:34:29 +0530

    product modified

--- controllers/productController.js
+++ controllers/productController.js
@@ -63,7 +63,7 @@ const deleteProduct = async (req, res) => {
 const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
-    res.status(200).json(products);
+    res.status(200).json({products});
   } catch (error) {
     res.status(500).json({ message: "Error fetching products", error });
   }
