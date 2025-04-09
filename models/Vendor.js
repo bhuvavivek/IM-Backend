5811db102193a157c7b0ad67d1b62205c8e76982
bhuvavivek <bhuvavivek67@gmaill.com>
2025-04-09 21:53:55 +0530

    custom vendoer and stock controller modified and westage section added

--- models/Vendor.js
+++ models/Vendor.js
@@ -1,24 +1,34 @@
 import mongoose from "mongoose";
-
 const VendorSchema = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    contact: { type: String, required: true },
+    contact: { type: Number, required: true },
     email: { type: String, required: true, unique: true },
     billingAddress: {
       country: { type: String, required: true },
       Address: { type: String, required: true },
       city: { type: String, required: true },
       state: { type: String, required: true },
-      pinCode: { type: String, required: true },
+      pinCode: { type: Number, required: true },
     },
     shippingAddress: {
       country: { type: String, required: true },
       Address: { type: String, required: true },
       city: { type: String, required: true },
       state: { type: String, required: true },
-      pinCode: { type: String, required: true },
+      pinCode: { type: Number, required: true },
+    },
+    transportInfo: {
+      companyName: { type: String },
+      contactNumber: { type: Number },
+      address: { type: String },
+    },
+    bankInfo: {
+      bankName: { type: String },
+      accountNumber: { type: Number },
+      ifscCode: { type: String },
+      accountHolderName: { type: String },
     },
   },
   { timestamps: true }
